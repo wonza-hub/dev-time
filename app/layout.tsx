e42d2ff62af782fb346c7adc0fc22ab6d45b2786
wonza-hub <wonza8585@gmail.com>
2026-02-07 23:16:14 +0900

    refactor: html 기본 언어 en->ko 변경 #1

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body className={`${pretendard.className}`}>{children}</body>
     </html>
   );
