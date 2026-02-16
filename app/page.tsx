103f5b68cd5f65ead09597aa2e2c3fbf19a74a5a
wonza-hub <wonza8585@gmail.com>
2026-02-16 17:26:35 +0900

    feat: 얼럿 모달 테스트 페이지 추가 #5

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
+'use client';
+
+import { Button } from '@/shared/ui';
+import { AlertDialogTrigger } from '@/shared/ui/alert-dialog';
+import AlertModal from '@/widgets/alert-modal/ui/alert-modal';
+import { toast } from 'sonner';
+
 export default function Home() {
   return (
-    <div className="text-3xl font-medium flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-black">
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 text-3xl font-medium dark:bg-black">
       프리텐다드
+      <AlertDialogTrigger asChild>
+        <Button variant="outline">얼럿 모달 테스트</Button>
+      </AlertDialogTrigger>
+      <AlertModal
+        title="알림"
+        body="알림 내용"
+        onCancel={() => {
+          toast.error('취소');
+        }}
+        onAction={() => {
+          toast.success('확인');
+        }}
+      />
     </div>
   );
 }
